dc8455343d64a0fd995cd3dd3c48f43829314669
Jose Armando Altamira Martinez <87738592+jarmandoalt@users.noreply.github.com>
2026-07-15 04:02:00 +0000

    input upgrade

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 
 //let socket = io('https://server-who-is-this-pokmeon.onrender.com')
 //let socket = io('//localhost:5050')
-const socket = io("https://sturdy-palm-tree-gw5p576g9g7cwrq6-5050.app.github.dev/", {
+const socket = io("https://fictional-space-doodle-v7g5gq5v65p36pv5-5050.app.github.dev/", {
   transports: ["websocket"], 
   upgrade: false
 });
